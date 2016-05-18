376ed303908b74ef0761c7d087d9d3f363717da6
kevinwforwork <kevinwforwork@163.com>
2016-05-18 08:23:07 +0800

    创建新文件

---
+++ 百思不得姐/Classes/FriendTrends(关注)/Controller/KWGitTest.h
@@ -0,0 +1,13 @@
+//
+//  KWGitTest.h
+//  百思不得姐
+//
+//  Created by 王鑫 on 16/5/18.
+//  Copyright © 2016年 KW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KWGitTest : NSObject
+
+@end
---
+++ 百思不得姐/Classes/FriendTrends(关注)/Controller/KWGitTest.m
@@ -0,0 +1,13 @@
+//
+//  KWGitTest.m
+//  百思不得姐
+//
+//  Created by 王鑫 on 16/5/18.
+//  Copyright © 2016年 KW. All rights reserved.
+//
+
+#import "KWGitTest.h"
+
+@implementation KWGitTest
+
+@end
