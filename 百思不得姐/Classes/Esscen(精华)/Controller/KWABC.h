b83b0cc4a4a560a4ade31536ef7dffe4df2b52d8
kevinwforwork <kevinwforwork@163.com>
2016-05-20 09:34:13 +0800

    测试
    测试

---
+++ 百思不得姐/Classes/Esscen(精华)/Controller/KWABC.h
@@ -0,0 +1,13 @@
+//
+//  KWABC.h
+//  百思不得姐
+//
+//  Created by 王鑫 on 16/5/20.
+//  Copyright © 2016年 KW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KWABC : UIView
+
+@end
---
+++ 百思不得姐/Classes/Esscen(精华)/Controller/KWABC.m
@@ -0,0 +1,21 @@
+//
+//  KWABC.m
+//  百思不得姐
+//
+//  Created by 王鑫 on 16/5/20.
+//  Copyright © 2016年 KW. All rights reserved.
+//
+
+#import "KWABC.h"
+
+@implementation KWABC
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
